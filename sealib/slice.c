c04384549cfab86dab0d55b954e12d23a23f7cfd
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-09-24 20:31:48 +0200

    sealib/slice: fixed slice growing

--- sealib/slice.c
+++ sealib/slice.c
@@ -9,32 +9,28 @@
 
 CsSlice *CsSliceNew(size_t initial_size) {
   CsSlice *s = malloc(sizeof(CsSlice));
-  s->elements = calloc(initial_size, sizeof(void));
+  s->elements = calloc(initial_size, sizeof(void *));
   s->cap = initial_size < SLICE_MIN_SIZE ? SLICE_MIN_SIZE : initial_size;
   s->len = 0;
   return s;
 }
 
-// TODO: i ran into so much issues while trying to implement this without
-// creating a new Slice, but i got so many invalid next sizes and corrupted
-// tops i gave up, so this is the best you get:
-/* static void grow_slice(CsSlice *s) { */
-/*   CsSlice *n = CsSliceNew(s->cap * 2); */
-/*   for (int i = 0; i < s->len; i++) { */
-/*     CsSliceAppend(s, s->elements[i]); */
-/*   } */
-/*   *s = *n; */
-/* } */
+static void grow_slice(CsSlice *s) {
+  void **t = calloc(s->cap * 2, sizeof(void *));
+  for (size_t i = 0; i < s->len; i++) {
+    t[i] = s->elements[i];
+  }
+  free(s->elements);
+  s->elements = t;
+  s->cap *= 2;
+}
 
 void CsSliceAppend(CsSlice *s, void *element) {
   // if we append and the new len is bigger or equal to the size we double the
   // slice, therefore we make a good tradeoff between times we need to grow
   // the slice and the amount of memory we take up
   if (s->len + 1 > s->cap) {
-    // TODO: we should grow the array here but i am too retarded to implement
-    // it, corrupted tops here, segmentation faults there
-    // grow_slice(s);
-    return;
+    grow_slice(s);
   }
 
   s->elements[s->len] = element;
